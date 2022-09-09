0b2258d407729c7357717fe43873b98d24361f6d
Alexander Friesen <friesen.alex.home@gmail.com>
2022-09-09 18:12:59 +0200

    add user reviews page

---
+++ src/routes/user/[id]/reviews/+page.server.ts
@@ -0,0 +1,51 @@
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+import { db } from '$lib/database';
+import { canViewUser } from '$lib/permissions';
+
+/** @type {import('./$types').PageServerLoad} */
+export const load: PageServerLoad = async ({ locals, params }) => {
+    const currentUser = locals.user;
+    const author_id = Number(params.id);
+
+    console.log(author_id)
+    if (author_id) {
+        const user = await db.user.findUnique({
+            where: { id: author_id },
+            select: {
+                username: true,
+                full_name: true,
+                is_admin: true,
+                is_blocked: true,
+            }
+        });
+        const reviews = await db.assetReview.findMany({
+            where: { author_id },
+            include: {
+                reply: true,
+                asset: true,
+                author: {
+                    select: {
+                        id: true,
+                        username: true,
+                    }
+                }
+            }
+        });
+
+
+        if (reviews) {
+            if (!canViewUser(currentUser, user)) {
+                throw error(403, 'Forbidden');
+            }
+
+            return {
+                user,
+                reviews,
+            };
+        }
+    }
+
+    throw error(404, 'Not found');
+};
---
+++ src/routes/user/[id]/reviews/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Date from '$lib/components/Date.svelte';
+	import { t } from '$lib/translations';
+
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="container px-0 sm:px-2">
+	<h1 class="text-center text-2xl font-medium mb-8">
+		{$t("{{user}}'s reviews", { user: data.user.username })}
+	</h1>
+
+	{#if data.reviews.length > 0}
+		<table class="w-full lg:w-3/4 xl:w-2/3 mx-auto shadow rounded text-sm">
+			<thead>
+				<tr class="font-bold">
+					<td class="bg-white border dark:bg-gray-800 px-3 py-1 text-right">{$t('Date')}</td>
+					<td class="bg-white border dark:bg-gray-800 px-3 py-1">{$t('Asset')}</td>
+					<td class="bg-white border dark:bg-gray-800 px-3 py-1">{$t('Review')}</td>
+				</tr>
+			</thead>
+			<tbody>
+				{#each data.reviews as review}
+					<tr class="bg-white dark:bg-gray-800">
+						<td class="border px-3 py-1 text-right">
+							<Date date={review.created_at} />
+						</td>
+
+						<td class="border px-3 py-1">
+							<a href="/asset/{review.asset.id}" class="link">
+								{review.asset.title}
+							</a>
+						</td>
+
+						<td class="border px-3 py-1">
+							{#if review.is_positive}
+								<span class="font-bold text-blue-500 dark:text-blue-400">
+									<span class="-ml-1 fa fa-chevron-circle-up fa-fw opacity-75" />
+									{$t('Recommended')}
+								</span>
+							{:else}
+								<span class="font-bold text-red-700 dark:text-red-600">
+									<span class="-ml-1 fa fa-chevron-circle-down fa-fw opacity-75" />
+									{$t('Not recommended')}
+								</span>
+							{/if}
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{:else}
+		<div class="mt-8 text-lg text-center text-gray-600">
+			{$t("This user hasn't posted any reviews yet.")}
+		</div>
+	{/if}
+</div>
